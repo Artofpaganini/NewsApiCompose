2db8002a1fc9869f396ef3c3673b4435cf09f39e
dobrov.v <dobrov.v@promail366.com>
2023-06-05 18:51:49 +0300

    qwe

--- app/src/main/java/com/example/newsapicompose/MainActivity.kt
+++ app/src/main/java/com/example/newsapicompose/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.newsapicompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.newsapicompose.ui.theme.NewsApiComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NewsApiComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                HomeScreen()
             }
         }
     }
@@ -29,6 +40,35 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+
+@Composable
+fun HomeScreen() {
+    Log.d("TAG", "HomeScreen")
+    val list = remember {
+        List(20) { "Item ${it + 1}" }.toMutableStateList()
+    }
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = Modifier
+            .border(width = 2.dp, color = Color.Green),
+        contentPadding = PaddingValues(16.dp),
+    ) {
+        items(list) { item ->
+            SomeItem(text = item)
+        }
+    }
+}
+
+@Composable
+fun SomeItem(text: String) {
+    Text(
+        text = text, fontSize = 20.sp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(width = 1.dp, color = Color.Black)
+            .padding(16.dp)
+    )
 }
 
 @Preview(showBackground = true)
